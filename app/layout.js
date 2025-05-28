d0b14a166f3c082783f6a7e2f7cca8d82a91ba57
rukonpro <rukon.js@gmail.com>
2025-05-29 01:04:11 +0600

    comment out layout

--- app/layout.js
+++ app/layout.js
@@ -26,7 +26,7 @@ export default function RootLayout({ children }) {
     return (
         <html lang="en" suppressHydrationWarning>
             <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-                {/*{children}*/}
+                {children}
             </body>
         </html>
     );
